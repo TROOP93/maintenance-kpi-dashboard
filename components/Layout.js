0cef17cb5623839f368683072082f5c2f3abc0b7
TROOP93 <d7.1996.au@gmail.com>
2026-02-28 05:59:00 +0300

    Add Sidebar component to Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,1 +1,17 @@
+import Sidebar from "./Sidebar";
 
+export default function Layout({ children }) {
+  return (
+    <div style={{ display: "flex", minHeight: "100vh", background: "#f4f6f9" }}>
+      
+      {/* Main Content */}
+      <div style={{ flex: 1, padding: "30px" }}>
+        {children}
+      </div>
+
+      {/* Sidebar on RIGHT */}
+      <Sidebar />
+
+    </div>
+  );
+}
